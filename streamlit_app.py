78e0f39311e7c0dde7d8d291400fb0615116281a
robert-arc <143133253+robert-arc@users.noreply.github.com>
2024-05-11 18:46:37 -0400

    Update streamlit_app.py
    Testing

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize your Smoothie :cup_with_straw:")
@@ -14,7 +15,14 @@ st.write('The name in your Smoothie will be:', name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+
+# Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
@@ -27,7 +35,7 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
 	    ingredients_string += fruit_chosen + ' '
-	    st.subheader(fruit_chosen + 'Nutrition Information')
+	    st.subheader(fruit_chosen + ' - Nutrition Information')
 	    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
 	    fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
    
