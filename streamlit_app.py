a43244f301c5c2586697be385928e89e06313765
robert-arc <143133253+robert-arc@users.noreply.github.com>
2024-05-11 17:48:19 -0400

    Update streamlit_app.py
    Rearranging code lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize your Smoothie :cup_with_straw:")
@@ -14,7 +15,6 @@ st.write('The name in your Smoothie will be:', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
@@ -23,31 +23,20 @@ ingredients_list = st.multiselect(
     )
 
 if ingredients_list:
-    #st.write(ingredients_list)
-    #st.text(ingredients_list)
-
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
-
-    #st.write(ingredients_string)
-
+	    ingredients_string += fruit_chosen + ' '
+	    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+	    fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+   
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
 
-    #st.write(my_insert_stmt)
-    #st.stop()
-    
+	
     time_to_insert = st.button('Submit Order')
 
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
 	   
         st.success('Your Smoothie is ordered,  '+name_on_order+'!', icon="✅")
-
-# New section to display fruityvice nutrition information
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
